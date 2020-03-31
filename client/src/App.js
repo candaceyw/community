cd809d1a1a4df6a16e3c8f9e920349cd41585759
Candace Wilson <candaceyw@gmail.com>
2020-03-31 18:17:43 -0400

    file setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './components/layout/Navbar'
+
 function App() {
-  return <div className='App'>My community app</div>;
-}
+  return (
+  <div className='App'>
+    <Navbar />
+
+
+  </div>
+  )}
 
 export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        
+  <nav >
+  <div class="nav-wrapper blue darken-3" >
+    <a href="#" class="brand-logo">Logo</a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+      <li><a href="sass.html">Healthcare Provider</a></li>
+      <li><a href="badges.html">Community</a></li>
+      <li><a href="collapsible.html">Resources</a></li>
+      <li><a href="collapsible.html">About</a></li>
+      <li><a href="collapsible.html">Contact</a></li>
+
+
+    </ul>
+  </div>
+</nav>
+    )
+}
+
+export default Navbar
